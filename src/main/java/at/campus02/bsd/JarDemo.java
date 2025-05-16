0c9929c6616b9e9260a79a6bfd2b7e1bfe877e19
benjaminpuja <benjaminpuja@icloud.com>
2025-05-16 16:41:18 +0200

    creating conflict

--- src/main/java/at/campus02/bsd/JarDemo.java
+++ src/main/java/at/campus02/bsd/JarDemo.java
@@ -4,6 +4,6 @@ public class JarDemo {
     public static void main(String[] args) {
         System.out.println("Hello Graz");
         System.out.println(args[0]);
-        return;
+        Boolean a = Boolean.parseBoolean(args[0]);
     }
 }
