626f05642f0115d12b4b11e629c20f9941d7da43
benjaminpuja <benjaminpuja@icloud.com>
2025-05-16 20:37:49 +0200

    changing project structure due to failing tests

--- src/test/java/at/campus02/bsd/CalculatorTest.java
+++ src/test/java/at/campus02/bsd/CalculatorTest.java
@@ -14,30 +14,30 @@ public class CalculatorTest {
 
     @Test
     void testAdd() {
-        Assertions.assertEquals( 5, calculator.add(2, 3) );
-        Assertions.assertEquals( 0, calculator.add(-1, 1) );
-        Assertions.assertEquals(-5, calculator.add(-2, -3) );
+        Assertions.assertEquals( 5.0, calculator.add(2.0, 3.0) );
+        Assertions.assertEquals( 0.0, calculator.add(-1.0, 1.0) );
+        Assertions.assertEquals(-5.0, calculator.add(-2.0, -3.0) );
     }
 
     @Test
     void testSub() {
-        Assertions.assertEquals(-1, calculator.subtract(2, 3) );
-        Assertions.assertEquals( 2, calculator.subtract(1, -1) );
-        Assertions.assertEquals( 0, calculator.subtract(3, 3) );
+        Assertions.assertEquals(-1.0, calculator.subtract(2.0, 3.0) );
+        Assertions.assertEquals( 2.0, calculator.subtract(1.0, -1.0) );
+        Assertions.assertEquals( 0.0, calculator.subtract(3.0, 3.0) );
     }
 
     @Test
     void testMul() {
-        Assertions.assertEquals( 6, calculator.multiply(2, 3) );
-        Assertions.assertEquals(-2, calculator.multiply(-1, 2) );
-        Assertions.assertEquals( 0, calculator.multiply(0, 5) );
+        Assertions.assertEquals( 6.0, calculator.multiply(2.0, 3.0) );
+        Assertions.assertEquals(-2.0, calculator.multiply(-1.0, 2.0) );
+        Assertions.assertEquals( 0.0, calculator.multiply(0.0, 5.0) );
     }
 
     @Test
     void testDiv() {
-        Assertions.assertEquals( 2, calculator.divide(6, 3) );
-        Assertions.assertEquals( 0, calculator.divide(1, 2) );  // Integer-Division
+        Assertions.assertEquals( 2.0, calculator.divide(6.0, 3.0) );
+        Assertions.assertEquals( 0.5, calculator.divide(1.0, 2.0) );
         Assertions.assertThrows( ArithmeticException.class,
-                () -> calculator.divide(5, 0) );
+                () -> calculator.divide(5.0, 0.0) );
     }
 }
