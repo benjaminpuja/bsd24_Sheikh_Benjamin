04e36221a8c453af488d45576c7f2c60076144be
benjaminpuja <benjaminpuja@icloud.com>
2025-05-16 16:01:40 +0200

    Added a demo jar with Hello Graz and my name

---
+++ src/main/java/at/campus02/bsd/JarDemo.java
@@ -0,0 +1,8 @@
+package at.campus02.bsd;
+
+public class JarDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello Graz");
+        System.out.println(args[0]);
+    }
+}
